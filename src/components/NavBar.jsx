29cc689bc4e1626e6d0dd3f14cf0a51a4d588ae0
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-05-17 00:49:14 +0600

    nav & footer ready

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,73 @@
+"use client";
+import { Avatar, Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+const Navbar = () => {
+
+
+  return (
+  <div className="bg-white py-3">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto">
+      <ul className="flex gap-3">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/destinations"}>Destinations</Link>
+        </li>
+        <li>
+          <Link href={"/my-bookings"}>My Bookings</Link>
+        </li>
+
+        <li>
+          <Link href={"/add-destination"}>Add Destination</Link>
+        </li>
+      </ul>
+
+      <div>
+        <Image
+          src={"/assets/Wanderlast.png"}
+          height={150}
+          width={150}
+          alt="logo"
+        />
+      </div>
+
+      <ul className="flex items-center gap-3">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+
+        {/* {user ? (
+          <>
+            <li>
+              <Avatar>
+                <Avatar.Image referrerPolicy="no-referrer" alt="John Doe" src={user?.image} />
+                <Avatar.Fallback>{nsme}</Avatar.Fallback>
+              </Avatar>
+            </li>
+            <li>
+              <Button size="sm" onClick={handleSignOut} variant="danger" className={"rounded-none"}>
+                Logout
+              </Button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link href={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link href={"/signup"}>Sign Up</Link>
+            </li>
+          </>
+        )} */}
+      </ul>
+    </nav>
+  </div>
+  );
+};
+
+export default Navbar;
